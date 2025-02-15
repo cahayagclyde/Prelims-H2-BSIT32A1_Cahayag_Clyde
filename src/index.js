86bcffd347c9227f750a7463cc0a9ca244dac42b
cahayagclyde <162082389+cahayagclyde@users.noreply.github.com>
2025-02-15 09:58:18 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+// Ensure the root element exists
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found!");
+}
